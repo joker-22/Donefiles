c70d68d2d5ebd0ab52a886e2cdedf84c3de2294d
joker-22 <vegesanakarthikvarma@gmail.com>
2016-10-19 19:06:12 +0530

    Add files via upload

---
+++ CF732B.java
@@ -0,0 +1,120 @@
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+
+public class CF732B {
+public static void main(String []args) throws IOException {
+      MyScanner in = new MyScanner();
+      out = new PrintWriter(new BufferedOutputStream(System.out));
+      //Start your solution here.
+      int n = in.nextInt();
+      int k = in.nextInt();
+      int [] ar = new int[n];
+      ar = in.nextIntAr(ar);
+      int count = 0;
+      for(int i=0;i<n-1;++i){
+    	  if(ar[i]+ar[i+1]>=k){
+    		  continue;
+    	  }
+    	  else{
+    		  int c = ar[i] +ar[i+1];
+    		  int d = k - c;
+    		  ar[i+1] += d;
+    		  count += d;
+    	  }
+      }
+      out.println(count);
+      for(int i=0;i<n;++i){
+    	  out.print(ar[i]+" ");
+      }
+      //End your solution here.       
+      out.close();
+      
+   }
+
+ 
+
+   //-----------PrintWriter for faster output---------------------------------
+   public static PrintWriter out;
+
+   //-----------MyScanner class for faster input----------
+   public static class MyScanner {
+      BufferedReader br;
+      StringTokenizer st;
+
+      public MyScanner() {
+         br = new BufferedReader(new InputStreamReader(System.in));
+      }
+
+      String next() {
+          while (st == null || !st.hasMoreElements()) {
+              try {
+                  st = new StringTokenizer(br.readLine());
+              } catch (IOException e) {
+                  e.printStackTrace();
+              }
+          }
+          return st.nextToken();
+      }
+
+      int nextInt() {
+          return Integer.parseInt(next());
+      }
+
+      long nextLong() {
+          return Long.parseLong(next());
+      }
+
+      double nextDouble() {
+          return Double.parseDouble(next());
+      }
+      char nextChar()  {
+    	  try {
+			return br.readLine().charAt(0);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return 0 ;
+      }
+      int [] nextIntAr(int [] ar){
+    	  for(int i=0;i<ar.length;++i){
+    		  ar[i] = Integer.parseInt(next());
+    	  }
+    	  return ar;
+      }
+      long [] nextLongAr(long [] ar){
+    	  for(int i=0;i<ar.length;++i){
+    		  ar[i] = Long.parseLong(next());
+    	  }
+    	  return ar;
+      }
+      double [] nextDoubleAr(double [] ar){
+    	  for(int i=0;i<ar.length;++i){
+    		  ar[i] = Double.parseDouble(next());
+    	  }
+    	  return ar;
+      }
+      String [] nextStringAr(String [] ar){
+    	  for(int i=0;i<ar.length;++i){
+    		  ar[i] = next();
+    	  }
+    	  return ar;
+      }
+
+      String nextLine(){
+          String str = "";
+      try {
+         str = br.readLine();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+      return str;
+      }
+
+   }
+}
