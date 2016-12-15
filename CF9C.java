2017f6e6e94275592f773c1f1bf4c4a88df7bdd0
joker-22 <vegesanakarthikvarma@gmail.com>
2016-12-15 19:22:20 +0530

    Add files via upload

---
+++ CF9C.java
@@ -0,0 +1,114 @@
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+
+public class CF9C {
+public static void main(String []args) {
+      MyScanner in = new MyScanner();
+      out = new PrintWriter(new BufferedOutputStream(System.out));
+      //Start your solution from here.
+      long n = in.nextLong();
+      String [] ar = new String[1023];
+      for(int i=0;i<ar.length;++i){
+    	  String s = Integer.toBinaryString(i+1);
+    	  ar[i] = s;
+    //	  out.println(s);
+      }
+      long [] br = new long[ar.length];
+      for(int i=0;i<br.length;++i){
+    	  br[i] = Long.parseLong(ar[i]);
+      }
+      long count = 0;
+      for(int i=0;i<br.length;++i){
+    	  if(n >= br[i]){
+    		  count++;
+    	  }
+    	  else{
+    		  break;
+    	  }
+      }
+      out.println(count);
+      //End your solution here.       
+      out.close();
+   }
+ 
+ 
+
+   //-----------PrintWriter for faster output---------------------------------
+   public static PrintWriter out;
+
+   //-----------MyScanner class for faster input----------
+   public static class MyScanner {
+      BufferedReader br;
+      StringTokenizer st;
+
+      public MyScanner() {
+         br = new BufferedReader(new InputStreamReader(System.in));
+      }
+
+      String next() {
+          while (st == null || !st.hasMoreElements()) {
+              try {
+                  st = new StringTokenizer(br.readLine());
+              } catch (IOException e) {
+                  e.printStackTrace();
+              }
+          }
+          return st.nextToken();
+      }
+
+      int nextInt() {
+          return Integer.parseInt(next());
+      }
+
+      long nextLong() {
+          return Long.parseLong(next());
+      }
+
+      double nextDouble() {
+          return Double.parseDouble(next());
+      }
+      char nextChar()  {
+    	  return next().charAt(0);
+      }
+      int [] nextIntAr(int [] ar){
+    	  for(int i=0;i<ar.length;++i){
+    		  ar[i] = Integer.parseInt(next());
+    	  }
+    	  return ar;
+      }
+      long [] nextLongAr(long [] ar){
+    	  for(int i=0;i<ar.length;++i){
+    		  ar[i] = Long.parseLong(next());
+    	  }
+    	  return ar;
+      }
+      double [] nextDoubleAr(double [] ar){
+    	  for(int i=0;i<ar.length;++i){
+    		  ar[i] = Double.parseDouble(next());
+    	  }
+    	  return ar;
+      }
+      String [] nextStringAr(String [] ar){
+    	  for(int i=0;i<ar.length;++i){
+    		  ar[i] = next();
+    	  }
+    	  return ar;
+      }
+
+      String nextLine(){
+          String str = "";
+      try {
+         str = br.readLine();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+      return str;
+      }
+
+   }
+}
